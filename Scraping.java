a0b6f38617dd276e3b5e95ba8ee4e5d77123e0b8
Pavlov Smpy <97945879+DryDevGin@users.noreply.github.com>
2022-01-19 05:13:27 -0600

    Update Scraping.java

--- Scraping.java
+++ Scraping.java
@@ -10,7 +10,8 @@ import org.jsoup.select.Elements;
 
 public class Scraping {
 	
-public static final String url = "https://twitter.com/pavlov_dev";
+	//agrega el url de la pagina que vas a raspar
+public static final String url = "";
 	
 	
 	public static void main (String args[]) {
@@ -21,20 +22,21 @@ public static final String url = "https://twitter.com/pavlov_dev";
 			// Obtengo el HTML de la web en un objeto Document
 			Document document = getHtmlDocument(url);
 			
+			//agregas el rango de clases donde vas a raspar en el dom
 			// Busco todas las historias de meneame que estan dentro de: 
-			Elements entradas = document.select("css-901oao css-16my406 r-18jsvk2 r-poiln3 r-b88u0q r-bcqeeo r-qvutc0");
-			System.out.println("Número de entradas en la página inicial de Jarroba: "+entradas.size()+"\n");
+			Elements entradas = document.select("").not("");
+			System.out.println("Número de entradas : "+entradas.size()+"\n");
 			
-			// Paseo cada una de las entradas
+			// Paseas cada una de las clases que vas a raspar
 			for (Element elem : entradas) {
-				String titulo = elem.getElementsByClass("r-qvutc0").toString();
-				String autor = elem.getElementsByClass("r-bcqeeo").toString();
-				String fecha = elem.getElementsByClass("r-qvutc0").text();
+				String likes = elem.getElementsByClass("").toString();
+				String visto = elem.getElementsByClass("").toString();
+				String nuevosSeguidores = elem.getElementsByClass("").text();
 				
-				System.out.println(titulo+"\n"+autor+"\n"+fecha+"\n\n");
+				System.out.println(likes+"\n"+visto+"\n"+nuevosSeguidores+"\n\n");
 				
-				// Con el método "text()" obtengo el contenido que hay dentro de las etiquetas HTML
-				// Con el método "toString()" obtengo todo el HTML con etiquetas incluidas
+				// Con el metodo "text()" se obtiene el contenido que hay dentro de las etiquetas HTML
+				// Con el metodo "toString()" se obtiene todo el HTML con etiquetas incluidas
 			}
 				
 		}else
@@ -44,15 +46,13 @@ public static final String url = "https://twitter.com/pavlov_dev";
 	
 	
 	/**
-	 * Con esta método compruebo el Status code de la respuesta que recibo al hacer la petición
+	 * Con esta metodo se comprueba el Status code de la respuesta que se recibe al hacer la peticion
 	 * EJM:
 	 * 		200 OK					300 Multiple Choices
 	 * 		301 Moved Permanently	305 Use Proxy
 	 * 		400 Bad Request			403 Forbidden
 	 * 		404 Not Found			500 Internal Server Error
 	 * 		502 Bad Gateway			503 Service Unavailable
-	 * @param url
-	 * @return Status Code
 	 */
 	public static int getStatusConnectionCode(String url) {
 		
@@ -61,17 +61,15 @@ public static final String url = "https://twitter.com/pavlov_dev";
 		try {
 			response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
 		} catch (IOException ex) {
-			System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
+			System.out.println("Excepcion al obtener el Status Code: " + ex.getMessage());
 		}
 		return response.statusCode();
 	}
 	
 	
 	/**
-	 * Con este método devuelvo un objeto de la clase Document con el contenido del
-	 * HTML de la web que me permitirá parsearlo con los métodos de la librelia JSoup
-	 * @param url
-	 * @return Documento con el HTML
+	 * Con este metodo devuelvo un objeto de la clase Document con el contenido del
+	 * HTML de la web que me permitira parsearlo con los métodos de la librelia JSoup
 	 */
 	public static Document getHtmlDocument(String url) {
 
@@ -80,7 +78,7 @@ public static final String url = "https://twitter.com/pavlov_dev";
 		try {
 			doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
 		} catch (IOException ex) {
-			System.out.println("Excepción al obtener el HTML de la página" + ex.getMessage());
+			System.out.println("Excepcion al obtener el HTML de la pagina" + ex.getMessage());
 		}
 
 		return doc;
